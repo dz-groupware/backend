da41048661e351000702f87f2bb79dcdf57057fe
JE IN KIM <wpdls4332@gmail.com>
2023-09-15 09:24:34 +0900

    [feat]: 사원관리페이지 추가 및 회사관리페이지 트리입력수정
    <body>
    
    <footer>

--- src/main/java/com/example/backend/employeemgmt/dto/EmployeeMgmtReqDto.java
+++ src/main/java/com/example/backend/employeemgmt/dto/EmployeeMgmtReqDto.java
@@ -1,29 +1,29 @@
-//package com.example.backend.employeemgmt.dto;
-//
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//
-//import java.util.Date;
-//
-//public class EmployeeMgmtReqDto {
-//    private Long id;
-//    private String imageUrl;
-//    private String name;
-//    private String empIdNum;
-//    private String gender;
-//    private Boolean accountYn;
-//    private String loginId;
-//    private String loginPw;
-//    private String email;
-//    private String privEmail;
-//    private String mobileNumber;
-//    private String homeNumber;
-//    private String address;
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-//    private Date joinDate;
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-//    private Date resignationDate;
-//    public void setId(Long id) {
-//        this.id=id;
-//    }
-//}
-//
+package com.example.backend.employeemgmt.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public class EmployeeMgmtReqDto {
+    private Long id;
+    private String imageUrl;
+    private String name;
+    private String empIdNum;
+    private String gender;
+    private Boolean accountYn;
+    private String loginId;
+    private String loginPw;
+    private String email;
+    private String privEmail;
+    private String mobileNumber;
+    private String homeNumber;
+    private String address;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date joinDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date resignationDate;
+    public void setId(Long id) {
+        this.id=id;
+    }
+}
+
--- src/main/java/com/example/backend/employeemgmt/service/EmployeeMgmtServiceImpl.java
+++ src/main/java/com/example/backend/employeemgmt/service/EmployeeMgmtServiceImpl.java
@@ -1,49 +1,27 @@
-//package com.example.backend.employeemgmt.service;
-//
-//import com.example.backend.employee.mapper.EmployeeMapper;
-//import com.example.backend.employeemgmt.dto.EmployeeMgmtListResDto;
-//import com.example.backend.employeemgmt.dto.EmployeeMgmtReqDto;
-//import com.example.backend.employeemgmt.dto.EmployeeMgmtResDto;
-//import com.example.backend.employeemgmt.mapper.EmployeeMgmtMapper;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class EmployeeMgmtServiceImpl implements EmployeeMgmtService{
-//
-//    private final EmployeeMgmtMapper employeeMgmtMapper;
-//    public EmployeeMgmtServiceImpl(EmployeeMgmtMapper employeeMgmtMapper){
-//        this.employeeMgmtMapper = employeeMgmtMapper;
-//    }
-//    @Override
-//    public List<EmployeeMgmtListResDto> getEmployeeMgmtList() {
-//        return employeeMgmtMapper.getEmployeeMgmtList();
-//    }
-//
-//    @Override
-//    public EmployeeMgmtResDto getEmployeeDetailsById(Long id) {
-//        return employeeMgmtMapper.getEmployeeDetailsById(id);
-//    }
-//
-//    @Override
-//    public List<EmployeeMgmtListResDto> findEmployeeMgmtList(String compId, String searchType) {
-//        return employeeMgmtMapper.findEmployeeMgmtList(compId,searchType);
-//    }
-//
-//    @Override
-//    public void addEmployeeMgmt(EmployeeMgmtReqDto employeeMgmt) {
-//        employeeMgmtMapper.addEmployeeMgmt(employeeMgmt);
-//    }
-//
-//    @Override
-//    public void modifyEmployeeMgmt(Long id, EmployeeMgmtReqDto employeeMgmt) {
-//        employeeMgmt.setId(id);
-//        employeeMgmtMapper.modifyEmployeeMgmt(employeeMgmt);
-//    }
-//
-//    @Override
-//    public void removeEmployeeMgmt(Long id) {
-//        employeeMgmtMapper.removeEmployeeMgmt(id);
-//    }
-//}
+package com.example.backend.employeemgmt.service;
+
+import com.example.backend.companymgmt.dto.CompanyMgmtListResDto;
+import com.example.backend.config.jwt.SecurityUtil;
+import com.example.backend.employee.mapper.EmployeeMapper;
+import com.example.backend.employeemgmt.dto.EmployeeMgmtListResDto;
+import com.example.backend.employeemgmt.dto.EmployeeMgmtReqDto;
+import com.example.backend.employeemgmt.dto.EmployeeMgmtResDto;
+import com.example.backend.employeemgmt.mapper.EmployeeMgmtMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeMgmtServiceImpl implements EmployeeMgmtService{
+
+    private final EmployeeMgmtMapper employeeMgmtMapper;
+    public EmployeeMgmtServiceImpl(EmployeeMgmtMapper employeeMgmtMapper){
+        this.employeeMgmtMapper = employeeMgmtMapper;
+    }
+
+    @Override
+    public List<EmployeeMgmtListResDto> getEmployeeMgmtList() {
+        Long companyId = SecurityUtil.getCompanyId();
+        return employeeMgmtMapper.getEmployeeMgmtList(companyId);
+    }
+}
