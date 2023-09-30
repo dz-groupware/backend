ee97500c640529a4c1693505d624aaeec374435b
subo <kws3363@gmail.com>
2023-09-30 23:15:27 +0900

    [test]: 도메인 명시 테스트

--- src/main/java/com/example/backend/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/backend/config/jwt/JwtTokenProvider.java
@@ -92,6 +92,7 @@ public class JwtTokenProvider {
     cookie.setPath("/");
     if (useHttps) {
       cookie.setSecure(true);
+      cookie.setDomain(".amaranth2023.com");
     }
     response.addCookie(cookie);
   }
