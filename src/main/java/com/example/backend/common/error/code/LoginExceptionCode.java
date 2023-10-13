65fc236ceb61edba8c1db69dd440d163de6f581b
subo <kws3363@gmail.com>
2023-10-13 14:11:29 +0900

    [fix]: 로그인 에러코드 수정
    <body>
    
    <footer>

--- src/main/java/com/example/backend/common/error/code/LoginExceptionCode.java
+++ src/main/java/com/example/backend/common/error/code/LoginExceptionCode.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum LoginExceptionCode implements ExceptionCode {
-  UNMATCHED_PASSWORD(402, "비밀번호가 일치하지 않습니다."),
-  ID_NOT_FOUND(402, "해당하는 아이디가 없습니다."),
+  UNMATCHED_PASSWORD(405, "비밀번호가 일치하지 않습니다."),
+  ID_NOT_FOUND(405, "해당하는 아이디가 없습니다."),
   DISABLED(402, "사용할 수 없는 계정입니다."),
   ACCOUNT_EXPIRED(402,"만료된 계정입니다.");
 
